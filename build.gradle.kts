2a659a8c0a7c6c002f829cdf82a7a737ef80ee50
Florian Hehlen <florian@hehlen.name>
2025-11-29 01:25:11 +0100

    chore(build): update build dependencies (#151)
    * chore(deps): update plugin org.jlleitschuh.gradle.ktlint to v14 (#133)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update plugin org.jetbrains.grammarkit to v2023 (#135)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update plugin org.jetbrains.grammarkit to v2023.3.0.1 (#136)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update gradle to v9.2.1 (#143)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update actions/checkout action to v6 (#144)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update plugin org.jetbrains.intellij.platform to v2.10.5 (#145)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(deps): update plugin org.jetbrains.changelog to v2.5.0 (#146)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,10 @@ import org.jlleitschuh.gradle.ktlint.tasks.KtLintFormatTask
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.2.21"
-    id("org.jetbrains.intellij.platform") version "2.10.4"
-    id("org.jetbrains.grammarkit") version "2022.3.2.2"
-    id("org.jlleitschuh.gradle.ktlint") version "13.1.0"
-    id("org.jetbrains.changelog") version "2.4.0"
+    id("org.jetbrains.intellij.platform") version "2.10.5"
+    id("org.jetbrains.grammarkit") version "2023.3.0.1"
+    id("org.jlleitschuh.gradle.ktlint") version "14.0.1"
+    id("org.jetbrains.changelog") version "2.5.0"
 }
 
 group = providers.gradleProperty("pluginGroup").get()
